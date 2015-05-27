b1db032ff40921a4f81dc14d883eb8bb20c92905
Florian Haag <fhaag@users.sourceforge.net>
2015-05-28 01:15:58 +0200

    + introduced a central version source file
    Signed-off-by: Florian Haag <fhaag@users.sourceforge.net>

--- src/NameBasedGrid/Properties/AssemblyInfo.cs
+++ src/NameBasedGrid/Properties/AssemblyInfo.cs
@@ -39,21 +39,11 @@ using System.Windows;
 [assembly: AssemblyDescription("A library that provides a WPF grid panel whose columns and rows can be referred to by name.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Name-Based Grid")]
-[assembly: AssemblyCopyright("Copyright 2015 by Florian Haag")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
 [assembly: ComVisible(false)]
-
-// The assembly version has following format :
-//
-// Major.Minor.Build.Revision
-//
-// You can specify all the values or you can use the default the Revision and 
-// Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
--- src/Samples/NameBasedGridExample/Properties/AssemblyInfo.cs
+++ src/Samples/NameBasedGridExample/Properties/AssemblyInfo.cs
@@ -38,19 +38,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NameBasedGridExample")]
-[assembly: AssemblyCopyright("Copyright 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
 [assembly: ComVisible(false)]
-
-// The assembly version has following format :
-//
-// Major.Minor.Build.Revision
-//
-// You can specify all the values or you can use the default the Revision and 
-// Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
