2edc043c5a65661f16068bd831adf993cb417581
Florian Haag <fhaag@users.sourceforge.net>
2015-05-28 00:37:11 +0200

    + dynamic grid example
    Signed-off-by: Florian Haag <fhaag@users.sourceforge.net>

---
+++ src/DynamicGridExample/RemoveButton.cs
@@ -0,0 +1,64 @@
+﻿/*
+------------------------------------------------------------------------------
+This source file is a part of Name-Based Grid.
+
+Copyright (c) 2015 Florian Haag
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+------------------------------------------------------------------------------
+ */
+using System;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace NameBasedGrid.Examples.Dynamic
+{
+	internal class RemoveButton : Button
+	{
+		public RemoveButton(NameBasedGrid owner, long number)
+		{
+			if (owner == null) {
+				throw new ArgumentNullException("owner");
+			}
+			
+			this.owner = owner;
+			
+			this.column = "c" + number;
+			this.row = "r" + number;
+			NameBasedGrid.SetColumn(this, column);
+			NameBasedGrid.SetRow(this, row);
+			this.Content = number;
+			
+			owner.Children.Add(this);
+		}
+		
+		private readonly NameBasedGrid owner;
+		
+		private readonly string column, row;
+		
+		protected override void OnClick()
+		{
+			owner.Children.Remove(this);
+			owner.ColumnDefinitions.Remove(owner.ColumnDefinitions.First(cr => cr.Name == column));
+			owner.RowDefinitions.Remove(owner.RowDefinitions.First(cr => cr.Name == row));
+			
+			base.OnClick();
+		}
+	}
+}
