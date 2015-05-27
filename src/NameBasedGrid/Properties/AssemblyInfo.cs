c11ecc1dfd3f2c9940e510cbb60a278b08e9ff17
Florian Haag <fhaag@users.sourceforge.net>
2015-05-28 00:16:17 +0200

    * edited the AssemblyInfo file
    Signed-off-by: Florian Haag <fhaag@users.sourceforge.net>

--- src/NameBasedGrid/Properties/AssemblyInfo.cs
+++ src/NameBasedGrid/Properties/AssemblyInfo.cs
@@ -1,4 +1,29 @@
-﻿#region Using directives
+﻿/*
+------------------------------------------------------------------------------
+This source file is a part of Name-Based Grid.
+
+Copyright (c) 2015 Florian Haag
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+------------------------------------------------------------------------------
+ */
+#region Using directives
 
 using System;
 using System.Reflection;
@@ -10,12 +35,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NameBasedGrid")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Name-Based Grid Core Library")]
+[assembly: AssemblyDescription("A library that provides a WPF grid panel whose columns and rows can be referred to by name.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NameBasedGrid")]
-[assembly: AssemblyCopyright("Copyright 2015")]
+[assembly: AssemblyProduct("Name-Based Grid")]
+[assembly: AssemblyCopyright("Copyright 2015 by Florian Haag")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
