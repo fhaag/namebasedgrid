1cdce28ecbf4478ad54c818a51150eb2f7127a5f
Florian Haag <fhaag@users.sourceforge.net>
2015-05-27 23:25:46 +0200

    + namespace documentation
    Signed-off-by: Florian Haag <fhaag@users.sourceforge.net>

---
+++ src/NameBasedGrid/NamespaceDoc.cs
@@ -0,0 +1,42 @@
+﻿/*
+------------------------------------------------------------------------------
+This source file is a part of Name-Based Grid.
+
+Copyright (c) 2015 Florian Haag
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+------------------------------------------------------------------------------
+ */
+using System;
+using System.Runtime.CompilerServices;
+
+namespace NameBasedGrid
+{
+	/// <summary>
+	/// This namespace contains types related to the <see cref="T:NameBasedGrid"/> panel.
+	/// </summary>
+	/// <remarks>
+	/// <para>This namespace provides the <see cref="T:NameBasedGrid"/> class, a <see cref="System.Windows.Controls.Grid"/>-like panel that allows for defining columns and rows that can be referred to by their names.</para>
+	/// <para>The namespace also contains the types required to define the columns and rows in the grid, <see cref="ColumnOrRowBase"/>, and its subclasses <see cref="ColumnOrRow"/> and <see cref="VirtualColumnOrRow"/>.</para>
+	/// </remarks>
+	[CompilerGenerated]
+	class NamespaceDoc
+	{
+	}
+}
