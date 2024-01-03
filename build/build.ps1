9ed2eb24cb5cd4e25c64caf398835f6bef6a5ebc
Florian Haag <fhaag@users.sourceforge.net>
2024-01-03 23:42:31 +0100

    added some package settings

--- build/build.ps1
+++ build/build.ps1
@@ -3,6 +3,7 @@ Param(
 )
 
 $scriptDir = $PSScriptRoot
+$rootDir = [System.IO.Path]::Combine($scriptDir, '..')
 
 $inkscapePath = $Env:INKSCAPE_PATH
 if (-not $inkscapePath) {
@@ -10,15 +11,25 @@ if (-not $inkscapePath) {
 	return
 }
 
-$logoPath = [System.IO.Path]::Combine($scriptDir, 'logo.svg')
-&"$inkscapePath" --export-type="png" --export-width=128 "$logoPath"
+$logoPath = [System.IO.Path]::Combine($rootDir, 'pubinfo', 'logo.svg')
+$logoDestPath = [System.IO.Path]::Combine($rootDir, 'pubinfo', 'logo128.png')
+&"$inkscapePath" --export-type="png" --export-width=128 -o "$logoDestPath" "$logoPath"
 
-$slnDir = [System.IO.Path]::Combine($scriptDir, '..', 'src')
+$slnDir = [System.IO.Path]::Combine($rootDir, 'src')
 $slnPath = [System.IO.Path]::Combine($slnDir, 'NameBasedGrid.sln')
 $pjPath = [System.IO.Path]::Combine($slnDir, 'NameBasedGrid', 'NameBasedGrid.csproj')
 
 &dotnet build "$slnPath" -c Release
 
+$nuspecTemplatePath = [System.IO.Path]::Combine($rootDir, 'pubinfo', 'NameBasedGrid.template.nuspec')
+$nuspecPath = [System.IO.Path]::Combine($rootDir, 'pubinfo', 'NameBasedGrid.nuspec')
 
+$nuspecDoc = New-Object -TypeName System.Xml.XmlDocument
+$nuspecDoc.Load($nuspecTemplatePath)
+
+$versionEl = $nuspecDoc.DocumentElement.SelectSingleNode('/package/metadata/version')
+$versionEl.InnerText = $version
+
+$nuspecDoc.Save($nuspecPath)
 
 &dotnet pack "$pjPath" -o "$scriptDir"
