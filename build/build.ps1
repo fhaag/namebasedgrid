70a73fa4e9d665e49b4bedc0fada364fea304ef2
Florian Haag <fhaag@users.sourceforge.net>
2024-01-07 12:00:45 +0100

    automatically increased build number in assembly file version

--- build/build.ps1
+++ build/build.ps1
@@ -7,7 +7,7 @@ $rootDir = [System.IO.Path]::Combine($scriptDir, '..')
 
 $inkscapePath = $Env:INKSCAPE_PATH
 if (-not $inkscapePath) {
-	Write-Output 'Inkscape path not set.'
+	Write-Host -ForegroundColor Red 'Inkscape path not set.'
 	return
 }
 
@@ -15,11 +15,21 @@ $logoPath = [System.IO.Path]::Combine($rootDir, 'pubinfo', 'logo.svg')
 $logoDestPath = [System.IO.Path]::Combine($rootDir, 'pubinfo', 'logo128.png')
 &"$inkscapePath" --export-type="png" --export-width=128 -o "$logoDestPath" "$logoPath"
 
+. '.\buildVersionHelper.ps1'
+
+# retrieve previous assembly file version
+$dllOutputPath = [System.IO.Path]::Combine($rootDir, 'bin', 'Release', 'net8.0-windows', 'NameBasedGrid.dll') # any compiled file will do, no matter which target
+$newFileVer = getFileVersion -path "$dllOutputPath" -incBuild $true
+if (-not $newFileVer) {
+	$newFileVer = "$version.0"
+}
+Write-Host -ForegroundColor Cyan "New build version: $newFileVer"
+
 $slnDir = [System.IO.Path]::Combine($rootDir, 'src')
 $slnPath = [System.IO.Path]::Combine($slnDir, 'NameBasedGrid.sln')
 $pjPath = [System.IO.Path]::Combine($slnDir, 'NameBasedGrid', 'NameBasedGrid.csproj')
 
-&dotnet build -p "Version=$version" -c Release "$slnPath"
+&dotnet build -p "Version=$version" -p "FileVersion=$newFileVer" -c Release "$slnPath"
 
 $nuspecTemplatePath = [System.IO.Path]::Combine($rootDir, 'pubinfo', 'NameBasedGrid.template.nuspec')
 $nuspecPath = [System.IO.Path]::Combine($rootDir, 'pubinfo', 'NameBasedGrid.nuspec')
